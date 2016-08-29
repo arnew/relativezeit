3ec18d08d0f25817d0a751063f1d80fa72d562ba
Arne Wichmann <arne.wichmann@tuhh.de>
2016-08-29 12:49:08 +0200

    initial glancing support

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <pebble.h>
 #include "main.h"
+#include <glancing/glancing.h>
 
 // todo list:
 // only redraw on changes, done
 // enable logging only on debug, done
 // disable accellerometer -> doesnt seem to be a problem
-#define DEBUG 0
+#define DEBUG 1
 
+static GlanceState glancing = 0;
 static unsigned int last_selection;
 static Window *s_main_window;
 
@@ -80,41 +82,47 @@ CountOnesFromInteger (uint64_t value)
 
 static double slow_adj = 7;
 
-inline float min(float a, float b) {
-  if (a<b) return a;
+inline float
+min (float a, float b)
+{
+  if (a < b)
+    return a;
   return b;
 }
 
-inline float max(float a, float b) {
-  if (a<b) return b;
+inline float
+max (float a, float b)
+{
+  if (a < b)
+    return b;
   return a;
 }
 
 void
-enforce_weights (precision_t * precision, weights_t * weights, float v, float w,
-		 float b)
+enforce_weights (precision_t * precision, weights_t * weights, float v,
+		 float w, float b)
 {
   int i = CountOnesFromInteger (precision->seconds >> 60),
     j = CountOnesFromInteger (precision->seconds << 4),
     k = CountOnesFromInteger (precision->minutes),
     l = CountOnesFromInteger (precision->hours),
     m = CountOnesFromInteger (precision->days);
- // if (i)
-    weights->a = (weights->a * max(0,(v + w*i / 4))) + b;
+  // if (i)
+  weights->a = (weights->a * max (0, (v + w * i / 4))) + b;
 //  if (j)
-    weights->b = (weights->b * max(0,(v + w*j / 56))) + b; 
+  weights->b = (weights->b * max (0, (v + w * j / 56))) + b;
 //  if (k && CountOnesFromInteger (precision->minutes>>30))
-    weights->c = (weights->c * max(0,(v + w*k / 60))) + b;
+  weights->c = (weights->c * max (0, (v + w * k / 60))) + b;
 //  if (l && CountOnesFromInteger (precision->hours>>12))
-    weights->d = (weights->d * max(0,(v + w*l / 24))) + b;
+  weights->d = (weights->d * max (0, (v + w * l / 24))) + b;
   //if (m && CountOnesFromInteger (precision->days>>15))
-    weights->e = (weights->e * max(0,(v + w* m / 31))) + b;
-  #if DEBUG
+  weights->e = (weights->e * max (0, (v + w * m / 31))) + b;
+#if DEBUG
   APP_LOG (APP_LOG_LEVEL_DEBUG, "weights: %i %i %i %i %i",
 	   (int) (10000 * weights->a), (int) (10000 * weights->b),
 	   (int) (10000 * weights->c), (int) (10000 * weights->d),
 	   (int) (10000 * weights->e));
-  #endif
+#endif
 }
 
 
@@ -123,28 +131,28 @@ touch_precision (precision_t * precision)
 {
   last_selection = -1;
   static int state = 0;
-  if(precision->seconds & 0xF000000000000000)
+  if (precision->seconds & 0xF000000000000000 && glancing != GLANCING_ACTIVE)
     {
-    #if DEBUG
-      APP_LOG(APP_LOG_LEVEL_DEBUG, "state: %i", state);
-    #endif
-      if(state == 1) 
-        {
-        #if DEBUG
-          APP_LOG(APP_LOG_LEVEL_DEBUG, "positive enforce weights");
-        #endif
-          enforce_weights (precision, &weights,1, 2, 1);
-        } 
-      else if (state >= 8) 
-        {
-         // memset(precision, 0x55, sizeof(precision_t));
-        
-          memset(&weights, 0, sizeof(weights_t));
+#if DEBUG
+      APP_LOG (APP_LOG_LEVEL_DEBUG, "state: %i", state);
+#endif
+      if (state == 1)
+	{
+#if DEBUG
+	  APP_LOG (APP_LOG_LEVEL_DEBUG, "positive enforce weights");
+#endif
+	  enforce_weights (precision, &weights, 1, 2, 1);
+	}
+      else if (state >= 8)
+	{
+	  // memset(precision, 0x55, sizeof(precision_t));
+
+	  memset (&weights, 0, sizeof (weights_t));
 //          weights.a = weights.b = weights.c = weights.d = weights.e = 1;
-        }
-      state ++;
+	}
+      state++;
     }
-  else if(!(state&top64))
+  else if (!(state & top64))
     state = 0;
   precision->seconds |= top64;
 }
@@ -153,22 +161,22 @@ touch_precision (precision_t * precision)
 void
 update_precision (precision_t * precision)
 {
-  enforce_weights (precision, &weights, 0.99999,-0.00005, 0);
-  
+  enforce_weights (precision, &weights, 0.99999, -0.00005, 0);
+
   shift_precision_seconds (precision);
 
-  if ((precision->second_count % 60)==0)
+  if ((precision->second_count % 60) == 0)
     {
       shift_precision_minutes (precision);
     }
-  if ((precision->minute_count % 60)==0)
+  if ((precision->minute_count % 60) == 0)
     {
       shift_precision_hours (precision);
     }
-  if ((precision->hour_count % 24)==0)
+  if ((precision->hour_count % 24) == 0)
     shift_precision_days (precision);
-  #if DEBUG
-  
+#if DEBUG
+
   APP_LOG (APP_LOG_LEVEL_DEBUG, "precision: %i:%u %i:%u %i:%u %i",
 	   CountOnesFromInteger (precision->seconds),
 	   (unsigned int) precision->second_count,
@@ -177,36 +185,36 @@ update_precision (precision_t * precision)
 	   CountOnesFromInteger (precision->hours),
 	   (unsigned int) precision->hour_count,
 	   CountOnesFromInteger (precision->days));
-  #endif
+#endif
 }
 
 
 static void
 update_time ()
-{ 
-  
+{
+
   // Get a tm structure
   time_t temp = time (NULL);
   double adjustor =
-    (CountOnesFromInteger (precision.seconds >> 60)/4.0 * (1+weights.a)) +
-    (CountOnesFromInteger (precision.seconds << 4)/56.0 * (1+weights.b)) +
-    (CountOnesFromInteger (precision.minutes)/60.0 * (1+weights.c)) +
-    (CountOnesFromInteger (precision.hours)/24.0 * (1+weights.d)) +
-    (CountOnesFromInteger (precision.days)/31.0 * (1+weights.e));
+    (CountOnesFromInteger (precision.seconds >> 60) / 4.0 * (1 + weights.a)) +
+    (CountOnesFromInteger (precision.seconds << 4) / 56.0 * (1 + weights.b)) +
+    (CountOnesFromInteger (precision.minutes) / 60.0 * (1 + weights.c)) +
+    (CountOnesFromInteger (precision.hours) / 24.0 * (1 + weights.d)) +
+    (CountOnesFromInteger (precision.days) / 31.0 * (1 + weights.e));
   //temp = ((temp + adjustor/2)/adjustor)*adjustor;
 
   // Write the current hours and minutes into a buffer
   char *format = NULL;
   TextLayer *layer = s_time_layer;
   slow_adj = (slow_adj * 0.99 + adjustor * 0.01);
-  unsigned int selection = (unsigned int) max(slow_adj, adjustor);
-    #if DEBUG
-  APP_LOG (APP_LOG_LEVEL_DEBUG, "relativity: %u %u %u", (unsigned int) adjustor,
-	   (unsigned int) slow_adj, last_selection);
-  #endif
-  if(selection < 8 && selection == last_selection )
+  unsigned int selection = (unsigned int) max (slow_adj, adjustor);
+#if DEBUG
+  APP_LOG (APP_LOG_LEVEL_DEBUG, "relativity: %u %u %u",
+	   (unsigned int) adjustor, (unsigned int) slow_adj, last_selection);
+#endif
+  if (selection < 8 && selection == last_selection)
     return;
-    
+
   switch (selection)
     {
     default:
@@ -276,24 +284,47 @@ update_time ()
 
   // Display this time on the TextLayer
   text_layer_set_text (layer, s_buffer);
-  last_selection =  selection;
+  last_selection = selection;
 
+}
+
+
+void
+glancing_callback (GlancingData * data)
+{
+  glancing = data->state;
+  switch (data->state)
+    {
+    case GLANCING_ACTIVE:
+      APP_LOG (APP_LOG_LEVEL_DEBUG, "glancing active");
+      break;
+    case GLANCING_TIMEDOUT:
+      APP_LOG (APP_LOG_LEVEL_DEBUG, "glancing timed out");
+      break;
+    case GLANCING_INACTIVE:
+    default:
+      APP_LOG (APP_LOG_LEVEL_DEBUG, "glancing inactive");
+      break;
+    }
 }
 
 static void
 second_tick (struct tm *tick_time, TimeUnits units_changed)
 {
-  if(units_changed & MINUTE_UNIT) last_selection = -1;
+  if (units_changed & MINUTE_UNIT)
+    last_selection = -1;
   update_precision (&precision);
+  if (glancing & GLANCING_ACTIVE)
+    touch_precision (&precision);
   update_time ();
 }
 
 static void
 tap_handler (AccelAxisType axis, int32_t direction)
 {
- #if DEBUG
+#if DEBUG
   APP_LOG (APP_LOG_LEVEL_DEBUG, "tap");
-  #endif
+#endif
   touch_precision (&precision);
   update_time ();
 }
@@ -301,9 +332,9 @@ tap_handler (AccelAxisType axis, int32_t direction)
 static void
 focus_handler (bool in_focus)
 {
-  #if DEBUG
+#if DEBUG
   APP_LOG (APP_LOG_LEVEL_DEBUG, "focus");
-  #endif
+#endif
   touch_precision (&precision);
   update_time ();
 }
@@ -356,6 +387,7 @@ main_window_load (Window * window)
   layer_add_child (window_layer, text_layer_get_layer (s_time_layer));
   layer_add_child (window_layer, text_layer_get_layer (s_timedetail_layer));
   layer_add_child (window_layer, text_layer_get_layer (s_timerough_layer));
+
 }
 
 static void
@@ -372,24 +404,25 @@ main_window_unload (Window * window)
 static void
 init ()
 {
-  #if DEBUG
+#if DEBUG
   APP_LOG (APP_LOG_LEVEL_DEBUG, "init");
 #endif
   // read state
-  if (persist_exists(0))
+  if (persist_exists (0))
     persist_read_data (0, &precision, sizeof (precision_t));
-  else 
-    memset(&precision, 0x55, sizeof(precision_t));
-        
-  if(persist_exists(1))
+  else
+    memset (&precision, 0x55, sizeof (precision_t));
+
+  if (persist_exists (1))
     persist_read_data (1, &weights, sizeof (weights_t));
-  else 
-    memset(&weights, 0, sizeof(weights_t));
+  else
+    memset (&weights, 0, sizeof (weights_t));
   touch_precision (&precision);
 
   // Register with TickTimerService
   tick_timer_service_subscribe (SECOND_UNIT, second_tick);
 
+  glancing_service_subscribe (5 * 1000, true, true, glancing_callback);
   accel_tap_service_subscribe (tap_handler);
   app_focus_service_subscribe (focus_handler);
   // Create main Window element and assign to pointer
